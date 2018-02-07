5b8ab9ff2ff8b7217dca10c3ff226fc135ad497b
Jan Dwyer <jand@cruzio.com>
2018-02-06 22:14:21 -0800

    Initial commit

---
+++ QuickSort/src/MakeQSort.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class MakeQSort {
+    public static void main (String args[]) {
+        System.out.println ("in MakeQSort.main");
+
+        QSort qp = new QSort();
+
+        /*  (1) Test instantiation of QSort object.
+        for (int i = 0; i <= qp.getCount(); i++){
+            if (i == qp.getCount()) {
+                System.out.println(qp.getAlq().get(i));
+            } else {
+                System.out.print(qp.getAlq().get(i) + ", ");
+            }
+        }  */
+
+        // (2) Test partition results on an instance of QSort.
+        // System.out.println("call partition");
+        // qp.partition(qp.getAlq(), 0, qp.getCount());
+
+        // (3) Need a new instance for a fresh test of qs method.
+        QSort qp2 = new QSort();
+        System.out.println("call qs");
+        qp.qs(qp2.getAlq(), 0, qp2.getCount());
+    }
+}
---
+++ QuickSort/src/QSort.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+
+public class QSort {
+    private ArrayList<Integer> alq;
+    private int count = 0;
+
+    public QSort() {
+        alq = new ArrayList();
+        alq.add(3);
+        alq.add(7);
+        alq.add(8);
+        alq.add(5);
+        alq.add(2);
+        alq.add(1);
+        alq.add(9);
+        alq.add(5);
+        alq.add(4);
+        count = (alq.size() - 1);
+    }
+
+
+    public ArrayList<Integer> getAlq() {
+        return alq;
+    }
+
+
+    public int getCount() {
+        return count;
+    }
+
+
+    public void qs (ArrayList<Integer> alQs, int lowPos, int highPos) {
+        System.out.println("in QSort.qs()");
+        System.out.println("lowPos: " + lowPos + ", highPos: " + highPos);
+        printAlq(alQs);
+
+        int endPos = (alQs.size() - 1);
+
+        if (lowPos < highPos) {
+            highPos = partition (alQs, lowPos, highPos);
+
+            System.out.println("Lower portion:  lowPos: " + lowPos +
+                    ", (highPos-1): " + (highPos-1));
+            qs (alQs, lowPos, (highPos-1));
+            System.out.println("After Lower portion:  highPos: " + highPos);
+            System.out.println();
+
+            System.out.println("Higher portion:  (highPos+1): " + (highPos+1) +
+                    ", highPos: " + highPos + ", endPos: " + endPos);
+            qs (alQs, (highPos+1), endPos);
+        }
+    }
+
+
+    public int partition (ArrayList<Integer> alQp, int lowPos, int highPos) {
+        System.out.println("in QSort.partition()");
+        System.out.println("lowPos: " + lowPos + ", highPos: " + highPos);
+        printAlq(alQp);
+
+        int pvPos = highPos;
+        int pVal = alQp.get(pvPos);
+        int pvPriorPos = (pvPos-1);
+        int pvPriorVal = alQp.get(pvPriorPos);
+        int iPos = lowPos;
+        int iVal = alQp.get(iPos);
+
+        System.out.println("Initial values - iPos: " + iPos + ", iVal: " + iVal +
+                ", pvPos: " + pvPos + ", pVal: " + pVal +
+                ", pvPriorPos: " + pvPriorPos + ", pvPriorVal: " + pvPriorVal);
+
+        for (int j = lowPos; j < highPos; j++) {
+            // test the condition, per j.
+            if (iVal > pVal) {
+                // swap i with pvPos-1, and pvPos-1 with pvPos.
+                alQp.set(iPos, pvPriorVal);
+                alQp.set(pvPriorPos, pVal);
+                alQp.set(pvPos, iVal);
+                // pvPos equals the sort-size of the array.
+                pvPos--;
+                pvPriorPos--;
+                // get new values.
+                pvPriorVal = alQp.get(pvPriorPos);
+                iVal = alQp.get(iPos);
+                // if you swap, don't increment i.
+            } else {
+                iPos++;
+                iVal = alQp.get(iPos);
+            }
+            System.out.print ("j: " + j + " - ");
+            printAlq(alQp);
+        }
+        // printAlq(alQp);
+        System.out.println("end partition - pvPos: " + pvPos);
+        System.out.println();
+        return pvPos;
+    }
+
+
+    public void printAlq(ArrayList<Integer> alQs) {
+        System.out.println("in QSort.printAlq()");
+        // System.out.println("0  1  2  3  4  5  6  7  8");
+        for (int i = 0; i <= count; i++){
+            if (i == count) {
+                System.out.println(alQs.get(i));
+                System.out.println();
+            } else {
+                System.out.print(alQs.get(i) + ", ");
+            }
+        }
+    }
+
+}
